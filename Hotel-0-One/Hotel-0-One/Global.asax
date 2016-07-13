d5bd20b59054f190f8695f20a05d2b10e5fc9aad
hp <hp@HP-PC>
2016-07-13 17:26:21 -0400

    001

---
+++ Hotel-0-One/Hotel-0-One/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hotel_0_One.MvcApplication" Language="C#" %>
---
+++ Hotel-0-One/Hotel-0-One/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hotel_0_One.Startup))]
+namespace Hotel_0_One
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
